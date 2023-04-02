a8607fcd391ccdbeb3faa2a0e0e3300f92d2422b
Tebogochonco <115028731+Tebogochonco@users.noreply.github.com>
2023-04-03 01:46:29 +0200

    update
    more updates made to the app

--- Script/script.js
+++ Script/script.js
@@ -1,78 +1,74 @@
-function addTask() {
-//code to invoke the functions
-  let form = document.getElementById("form");
-  let inputOne = document.getElementById("input 1").value;
-  let inputTwo = document.getElementById("input 2").value;
-  let notice = document.getElementById("notice");
-  // let tasks = document.getElementById("tasks"); 
+window.addEventListener('load', () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const date = document.querySelector("#new-task-date");
+  const list_el = document.querySelector("#tasks");
 
-//code to prefent the page from refreshing everytime the button is clicked
-  form.addEventListener("submit", (e) => {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log("button clicked");
-    addTask();
-  })
 
-  //the setup for my function using the else if statement
-  let addTask = () => {
-    if (inputOne !== "" && inputTwo !== "") {
+    const task = input.value;
 
-      notice.innerHTML = "";
-      console.log("success");
-      // taskData();
- 
-      //code to create the task elements
-      let newTask = [];
-      newTask.push(
-        inputOne,
-        inputTwo,
-      );
-      let liEl = document.createElement("li");
-      let theSpan = document.createElement("span");
-      let node1 = document.createTextNode(newTask[0] + "  ");
-      //let node2 = document.createTextNode(newTask[1] + "  ");
-      //let node3 = document.createTextNode(newTask[2] + "  ");
+    const task_el = document.createElement('div');
+    task_el.classList.add('task');
 
-      theSpan.appendChild(node1);
-     // theSpan.appendChild(node2);
-      // theSpan.appendChild(node3);
+    const task_content_el = document.createElement('div');
+    task_content_el.classList.add('content');
 
-      liEl.appendChild(theSpan);
-      document.getElementById("taskList").appendChild(liEl);
+    task_el.appendChild(task_content_el);
 
-      // console.log(newTask);
-      console.log(liEl);
+    const task_input_el = document.createElement('input');
+    task_input_el.classList.add('text');
+    task_input_el.type = 'text';
+    task_input_el.value = task;
+    task_input_el.setAttribute('readonly', 'readonly');
 
-    //code to add the action buttons with each task element
-      const myButtons =
-        `
-          <input name="q" hidden value="${newTask}"></input>
-          <button type="radio" id="complete" >Completed</button>
-          <button type="submit" id="edit" >Edit</button>
-          <button type="submit" onClick="deleteTask(this)">Delete</button>
-          `
-      document.getElementById("taskList").innerHTML += myButtons;
-      console.log(liEl);
+    task_content_el.appendChild(task_input_el);
 
-    }
-    else if (inputOne === "" && inputTwo !== "") {
-      notice.innerHTML = "Please insert all inputs!"
-      console.log("fail")
-    }
-    else if (inputOne !== "" && inputTwo === "") {
-      notice.innerHTML = "Please insert all inputs!"
-      console.log("fail")
-    }
-    else {
-      notice.innerHTML = "Please insert a task!"
-      console.log("fail")
-    }
+    const task_actions_el = document.createElement('div');
+    task_actions_el.classList.add('actions');
 
-}}
+    const task_completed_el = document.createElement('button');
+    task_completed_el.classList.add('completed');
+    task_completed_el.innerText = 'Done';
 
 
-//code to give the delete button its function
-let deleteTask = (e) => {
-  e.parentElement.parentElement.remove();
+    const task_edit_el = document.createElement('button');
+    task_edit_el.classList.add('edit');
+    task_edit_el.innerText = 'Edit';
 
-} 
+    const task_delete_el = document.createElement('button');
+    task_delete_el.classList.add('delete');
+    task_delete_el.innerText = 'Delete';
+
+    task_actions_el.appendChild(task_edit_el);
+    task_actions_el.appendChild(task_completed_el);
+    task_actions_el.appendChild(task_delete_el);
+
+    task_el.appendChild(task_actions_el);
+
+    list_el.appendChild(task_el);
+
+    input.value = '';
+
+    task_edit_el.addEventListener('click', (e) => {
+      if (task_edit_el.innerText.toLowerCase() == "edit") {
+        task_edit_el.innerText = "Save";
+        task_input_el.removeAttribute("readonly");
+        task_input_el.focus();
+      } else {
+        task_edit_el.innerText = "Edit";
+        task_input_el.setAttribute("readonly", "readonly");
+      }
+    });
+
+    task_completed_el.addEventListener('click', (e) => {
+      list_el.toggleAttribute("disabled").task_el;
+
+    });
+
+    task_delete_el.addEventListener('click', (e) => {
+      list_el.removeChild(task_el);
+    });
+  });
+});
